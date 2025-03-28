564e0ff7169d30de0a3bae2351a3c5b5942f70c3
Caitlyn Green <caitgreen20@outlook.com>
2025-03-27 22:08:58 -0400

    HTML & CSS for home page

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,7 +1,4 @@
-// This is where you should write all JavaScript
-// for your project. Remember a few things as you start!
-// - Use let or const for all variables
-// - Do not use jQuery - use JavaScript instead
-// - Do not use onclick - use addEventListener instead
-// - Run npm run test regularly to check autograding
-// - You'll need to link this file to your HTML :)
+// Find the thing to toggle
+
+const logoimg = document.querySelector('#logo');
+console.log(logoimg);
