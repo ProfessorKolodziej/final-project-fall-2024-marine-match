afdfb07af5fd593c2f278766dad5554b1c18cd70
Caitlyn Green <caitgreen20@outlook.com>
2025-03-30 23:19:09 -0400

    updates
    gameplay page, instruction page (mostly) functioning

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,4 +1,27 @@
-// Find the thing to toggle
+// Default: Show page 1, hide Page 2 & 3
+const instr1 = document.querySelector('.instr1')
 
-const logoimg = document.querySelector('#logo');
-console.log(logoimg);
+const instr2 = document.querySelector('.instr2')
+instr2.classList.toggle("hide");
+
+const instr3 = document.querySelector('.instr3')
+instr3.classList.toggle("hide");
+
+// Toggler from Page 1 to Page 2
+const instrButton1 = document.querySelector('#button1');
+instrButton1.addEventListener("click", instrToggle1);
+
+function instrToggle1() {
+    instr1.classList.replace("instr1", "hide");
+    instr2.classList.replace("hide", "instr2");
+}
+
+// Toggler from Page 2 to Page 3
+const instrButton2 = document.querySelector('#button2')
+console.log(instrButton2)
+instrButton2.addEventListener("click", instrToggle2);
+
+function instrToggle2() {
+    instr2.classList.replace("instr2", "hide")
+    instr3.classList.replace("hide", "instr3")
+}
