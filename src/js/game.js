5f380c11050f979e6c8252d9fd245fce27ef3d0e
Caitlyn Green <caitgreen20@outlook.com>
2025-04-26 00:09:20 -0400

    responsive styles - tablet & phone
    added vh vw units of measurement and media queries for phone.

--- src/js/game.js
+++ src/js/game.js
@@ -5,12 +5,16 @@ const audioBtn = document.querySelector('#audioBtn')
 audioBtn.addEventListener("click", mute);
 
 function mute() {
+    let imgSrc = audioBtn.attributes.src.nodeValue;
+    if (imgSrc == "images/music_on.svg") {
+        audioBtn.attributes.src.nodeValue = "images/music_off.svg";
+    } else {
+        audioBtn.attributes.src.nodeValue = "images/music_on.svg";
+    }
     audio.muted = !audio.muted;
 }
 
 // game win condition bools
-
-let error = false; // will trigger graphics change when a wrong answer is submitted
 
 const animalWin = {
   'seal': false,
@@ -31,7 +35,7 @@ const popup = document.querySelector('#popup')
 const animals = [
     {
         id: 'seal',
-        TTid: 'sealTT'
+        TTid: 'sealTT',
     },
     {
         id: 'seahorse',
@@ -39,7 +43,8 @@ const animals = [
     },
     {
         id: 'starfish',
-        TTid: 'starTT' },
+        TTid: 'starTT'
+    },
 ];
 
 animals.forEach(animal => {
@@ -108,15 +113,16 @@ console.log(drake)
 function onDrop(el, target, source) {
     console.log("The item is:", el);
     let animalID = el.attributes.id.nodeValue; // otherwise it just grabs the full HTML element
+    el.style.filter = "drop-shadow(8px 8px 8px rgb(0, 87, 102, 0.8))";
     console.log("The animal ID is:", animalID);
     console.log("The target of this item is:", target);
     let targetID = target.attributes.id.nodeValue;
     console.log("The target ID is:", targetID);
     console.log("The source of this item is:", source);
     if (targetID == 'ice' || targetID == 'coral') {
-        el.style.maxHeight = "18vh";
+        el.style.maxHeight = "20vh";
     } else {
-        el.style.maxHeight = "24vh";
+        el.style.maxHeight = "28vh";
     }
 
   // win conditions
@@ -143,7 +149,7 @@ const winTT = document.querySelector('#winTT')
 const errorTT = document.querySelector('#errorTT')
 
 function winWindow() {
-    winScreen.classList.replace("hide", "popup");
+    winScreen.classList.replace("hide", "winPopup");
     winTT.classList.replace("hide", "show");
     overlay.classList.replace("hide", "overlay");
 }
@@ -156,14 +162,16 @@ function errorWindow() {
 
 function checkWin() {
     if (Object.values(animalWin).every(x => x)) { // for every single animal win value, check that it's true
-        console.log('the player has won :)');
-        let error = false;
+        //console.log('the player has won :)');
         winWindow();
 
     } else {
-        console.log('the game is yet to be won...');
-        let error = true;
+        //console.log('the game is yet to be won...');
         errorWindow();
+        animals.forEach((animal => {
+            if (!animalWin[animal.id]) {
+                document.querySelector(`#${animal.id}`).style.filter = "drop-shadow(8px 8px 8px rgb(255, 0, 0, 0.8))";
+            }
+        }))
     }
-    //console.log(error)
 }
